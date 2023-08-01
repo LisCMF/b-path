1bd0f774bf4f9dfc961562e4401331f343bc38bb
Lis Medina Frey <lismedina@protonmail.com>
2023-08-01 14:12:41 -0400

    "file structure updates"

--- client/reducers/bpathReducer.js
+++ client/reducers/bpathReducer.js
@@ -1,4 +1,5 @@
 // have the storo with the state
 // the app and updates 
-  // uses switch and case to select the action (e.g., types.ADD_MARKET) and reassign the state with the needed information 
+  // uses switch and case to select the action (e.g., types.ADD_MARKET) and reassign the state with the needed information
+   
   
