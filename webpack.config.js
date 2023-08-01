6cf33271a1178c2a2d901b9dd594ed5cf1f67f94
Lis Medina Frey <lismedina@protonmail.com>
2023-08-01 14:20:36 -0400

    "Moved webpack.config, gitignore, packlock,  pack"

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+// don't forget to require path
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  devServer: {
+    static: {
+      directory: path.join(__dirname), // root directory main path
+    },
+
+    compress: true,
+    port: 8080,
+    proxy: { '/api/leaders': 'http://localhost:3000/' },
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV, // for any run mode built dev
+  // plugins: [new htmlWebpackPlugin(
+  //   { template: './index.html'})],
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+              ['@babel/preset-react', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
