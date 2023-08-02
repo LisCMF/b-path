9a7d4a4ac2a8958a4087de4e8af3b3c90c6f965e
Lis Medina Frey <lismedina@protonmail.com>
2023-08-01 22:49:24 -0400

    "Info Container Completed"

--- client/containers/infoContainer.jsx
+++ client/containers/infoContainer.jsx
@@ -3,8 +3,19 @@ import { useSelector } from 'react-redux';
 
 const InfoContainer = () => {
   return(
-    <div class="imageContainer">
-
+    <div className="imageContainer">
+        <div>
+            <img className="infoImage" src='https://ehq-production-us-california.imgix.net/2930b618d947ec9174d37ed41ca9c591b7b365f3/photos/images/000/003/914/original/GEDC0700.jpg?auto=compress'/>
+            <p className="ratedCategory" >safety</p>
+        </div>
+        <div>
+            <img className="infoImage" src='https://newyorkyimby.com/wp-content/uploads/2023/02/IMG-3521-1024x768.jpg'/> 
+            <p className="ratedCategory" >scenery</p>
+        </div>
+        <div>
+            <img className="infoImage" src='https://ddot.dc.gov/sites/default/files/dc/sites/ddot/page_content/images/_J_43049_original.jpg?itok=XuwsMy3Q'/>
+            <p className="ratedCategory" >infrastructure</p>
+        </div>     
     </div> 
   )
 }
